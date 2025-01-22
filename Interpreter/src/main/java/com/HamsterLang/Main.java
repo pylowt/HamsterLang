e2aadf186770ab4d6f40209d8641808ddaf5b5a5
Paul <paul.lowther01@hotmail.com>
2025-01-22 20:06:27 +0000

    Add scanner obj and get user input

--- Interpreter/src/main/java/com/HamsterLang/Main.java
+++ Interpreter/src/main/java/com/HamsterLang/Main.java
@@ -1,7 +1,21 @@
 package com.HamsterLang;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String user = System.getProperty("user.name");
+        System.out.println("Hello " + user + " This is the Hamster programming language!");
+        while (true)
+        {
+            System.out.print(">> ");
+            Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+            String userInput = myObj.nextLine();  // Read user input
+            if (Objects.equals(userInput, "exit"))
+                break;
+//            Repl.Repl.ProcessInput(userInput);
+        }
     }
 }
