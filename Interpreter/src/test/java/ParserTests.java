30b64f4887dc2279615815b0199d93bdd215b3da
Paul <paul.lowther01@hotmail.com>
2025-02-09 10:24:19 +0000

    Add return statements and string rep tests

--- Interpreter/src/test/java/ParserTests.java
+++ Interpreter/src/test/java/ParserTests.java
@@ -1,8 +1,11 @@
 import com.HamsterLang.Ast.ASTRoot;
+import com.HamsterLang.Ast.Identifier;
 import com.HamsterLang.Ast.ReturnStatement;
 import com.HamsterLang.Ast.VarStatement;
 import com.HamsterLang.Lexer.Lexer;
 import com.HamsterLang.Parser.Parser;
+import com.HamsterLang.Tokens.Token;
+import com.HamsterLang.Tokens.TokenTypes;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,14 +21,14 @@ public class ParserTests {
         program = parser.parseProgram();
         checkParserErrors();
     }
-    private void checkParserErrors()
-    {
+
+    private void checkParserErrors() {
         var errors = parser.errors;
         assertFalse(errors.size() != 0, "Parser Error: " + String.join(" ", errors));
     }
 
     @Test
-    void TestVarStatements(){
+    void TestVarStatements() {
         var input = """
                 var x = 5;
                 var y = 10;
@@ -35,26 +38,41 @@ public class ParserTests {
         initialise(input);
         var statements = program.statements;
         assertNotNull(statements);
-		assertEquals(3, statements.size());
-        for (int i = 0 ; i != expectedStatements.length ; i++) {
+        assertEquals(3, statements.size());
+        for (int i = 0; i != expectedStatements.length; i++) {
             var statement = assertInstanceOf(VarStatement.class, statements.get(i));
             assertEquals(expectedStatements[i], statement.name.value);
             assertEquals("var", statement.tokenLiteral());
         }
     }
-    
+
     @Test
-    void TestReturnStatements(){
-       var input = """
-               return 5;
-               return 10;
-               return 993322;
-               """;
-       initialise(input);
-       assertEquals(3, program.statements.size());
-        for (var stmt: program.statements) {
+    void TestReturnStatements() {
+        var input = """
+                return 5;
+                return 10;
+                return 993322;
+                """;
+        initialise(input);
+        assertEquals(3, program.statements.size());
+        for (var stmt : program.statements) {
             assertInstanceOf(ReturnStatement.class, stmt);
             assertEquals("return", stmt.tokenLiteral());
         }
+    }
+
+    @Test
+    void TestStringRepresentation() {
+        var varTok = new Token(TokenTypes.TokenType.VAR, "var");
+        var someVar = new Identifier(new Token(TokenTypes.TokenType.IDENT, "myVar"), "myVar");
+        var differentVar = new Identifier(
+                new Token(TokenTypes.TokenType.IDENT, "differentVar"),
+                "differentVar");
+        var stmt = new VarStatement(varTok, someVar, differentVar);
+        var program = new ASTRoot();
+        program.statements.add(stmt);
+        var expected = "var myVar = differentVar;";
+        var actual = program.string();
+        assertEquals(expected, actual);
     }
 }
