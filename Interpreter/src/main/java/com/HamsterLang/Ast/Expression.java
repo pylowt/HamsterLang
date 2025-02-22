4eadf4920acb1555b6a7c6212a1990d0c9b40f62
Paul <paul.lowther01@hotmail.com>
2025-02-22 15:00:34 +0000

    Add Expression class

---
+++ Interpreter/src/main/java/com/HamsterLang/Ast/Expression.java
@@ -0,0 +1,13 @@
+package com.HamsterLang.Ast;
+
+public class Expression implements ASTNode {
+    @Override
+    public String tokenLiteral() {
+        return null;
+    }
+
+    @Override
+    public String string() {
+        return null;
+    }
+}
