ce8bacf63145ae91728bcf88e1e3e3f4c7cabf61
Paul <paul.lowther01@hotmail.com>
2025-03-18 20:13:20 +0000

    Add ExpressionStatement

---
+++ Interpreter/src/main/java/com/HamsterLang/Ast/ExpressionStatement.java
@@ -0,0 +1,19 @@
+package com.HamsterLang.Ast;
+
+import com.HamsterLang.Tokens.Token;
+
+public class ExpressionStatement extends Statement{
+    public ExpressionStatement(Token token) {
+        super(token);
+    }
+     private Expression expression;
+
+
+    public Expression getExpression() {
+        return expression;
+    }
+
+    public void setExpression(Expression expression) {
+        this.expression = expression;
+    }
+}
