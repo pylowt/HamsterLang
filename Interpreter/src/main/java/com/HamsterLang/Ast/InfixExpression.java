eed3eccce20c0350abf99900b49c1f1a80979f73
Paul <paul.lowther01@hotmail.com>
2025-03-21 18:36:29 +0000

    Add InfixExpression class

---
+++ Interpreter/src/main/java/com/HamsterLang/Ast/InfixExpression.java
@@ -0,0 +1,21 @@
+package com.HamsterLang.Ast;
+
+import com.HamsterLang.Tokens.Token;
+
+public class InfixExpression extends Expression{
+    private Token token;
+    private Expression left;
+    private String operator;
+    private Expression right;
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public Expression getLeft() {
+        return left;
+    }
+    public Expression getRight() {
+        return right;
+    }
+}
