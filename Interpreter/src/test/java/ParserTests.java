93fadd3e3278eafb4ced36bbdd81ccba6663a355
Paul <paul.lowther01@hotmail.com>
2025-03-22 15:33:43 +0000

    Add missing tests from last chapter and setups

--- Interpreter/src/test/java/ParserTests.java
+++ Interpreter/src/test/java/ParserTests.java
@@ -95,6 +95,7 @@ public class ParserTests {
         assertEquals("foobar", tokenLiteral);
 
     }
+
     @Test
     void TestIntegerLiteralExpression() {
         var input = "5;";
@@ -114,85 +115,127 @@ public class ParserTests {
     }
 
 
-        // Test case class to hold input, expected operator, and expected integer value
-        static class TestCase {
-            String input;
-            String operator;
-            List<Integer> operands;
+    // Test case class to hold input, expected operator, and expected integer value
+    static class TestCase {
+        String input;
 
-            TestCase(String input, String operator, List<Integer> operands) {
-                this.input = input;
-                this.operator = operator;
-                this.operands = operands;
-            }
+        TestCase(String input) {
+            this.input = input;
         }
+    }
 
-    static Stream<TestCase> prefixTestCases() {
+    static class InfixPrefixTestCase extends TestCase {
+        String operator;
+        List<Integer> operands;
+
+        InfixPrefixTestCase(String input, String operator, List<Integer> operands) {
+            super(input);
+            this.operator = operator;
+            this.operands = operands;
+        }
+    }
+
+    static class OperatorPrecedenceTestCase extends TestCase {
+        String expected;
+
+        OperatorPrecedenceTestCase(String input, String expected) {
+            super(input);
+            this.expected = expected;
+        }
+    }
+
+    static Stream<InfixPrefixTestCase> prefixTestCases() {
         return Stream.of(
-                new TestCase("!5;", "!", List.of(5)),
-                new TestCase("-15;", "-", List.of(15))
+                new InfixPrefixTestCase("!5;", "!", List.of(5)),
+                new InfixPrefixTestCase("-15;", "-", List.of(15))
         );
     }
 
-        @ParameterizedTest
-        @MethodSource("prefixTestCases")
-        void testParsingPrefixExpressions(TestCase tt) {
-            initialise(tt.input);
+    @ParameterizedTest
+    @MethodSource("prefixTestCases")
+    void testParsingPrefixExpressions(InfixPrefixTestCase tt) {
+        initialise(tt.input);
 
-            // Assert that there is exactly one statement in the program
-            assertEquals(1, program.statements.size(), "program.Statements does not contain 1 statement");
+        // Assert that there is exactly one statement in the program
+        assertEquals(1, program.statements.size(), "program.Statements does not contain 1 statement");
 
-            var stmt = (ExpressionStatement) program.statements.get(0);
-            assertTrue(stmt.getExpression() instanceof PrefixExpression,
-                    "exp is not a PrefixExpression");
+        var stmt = (ExpressionStatement) program.statements.get(0);
+        assertTrue(stmt.getExpression() instanceof PrefixExpression,
+                "exp is not a PrefixExpression");
 
-            PrefixExpression exp = (PrefixExpression) stmt.getExpression();
-            assertEquals(tt.operator, exp.getOperator(), "exp.Operator does not match expected value");
+        PrefixExpression exp = (PrefixExpression) stmt.getExpression();
+        assertEquals(tt.operator, exp.getOperator(), "exp.Operator does not match expected value");
 
-            assertTrue(testIntegerLiteral(exp.getRight(), tt.operands.get(0)));
+        assertTrue(testIntegerLiteral(exp.getRight(), tt.operands.get(0)));
+    }
+
+    boolean testIntegerLiteral(Expression il, long value) {
+        if (!(il instanceof IntegerLiteral integ)) {
+            return false;
+        }
+        if (integ.getValue() != value) {
+            return false;
         }
 
-        boolean testIntegerLiteral(Expression il, long value) {
-            if (!(il instanceof IntegerLiteral integ)) {
-                return false;
-            }
-            if (integ.getValue() != value) {
-                return false;
-            }
+        return integ.tokenLiteral().equals(String.format("%d", value));
+    }
 
-            return integ.tokenLiteral().equals(String.format("%d", value));
-        }
+    static Stream<InfixPrefixTestCase> infixTestCases() {
+        return Stream.of(
+                new InfixPrefixTestCase("5 + 5;", "+", List.of(5, 5)),
+                new InfixPrefixTestCase("5 - 5;", "-", List.of(5, 5)),
+                new InfixPrefixTestCase("5 * 5;", "*", List.of(5, 5)),
+                new InfixPrefixTestCase("5 / 5;", "/", List.of(5, 5)),
+                new InfixPrefixTestCase("5 > 5;", ">", List.of(5, 5)),
+                new InfixPrefixTestCase("5 < 5;", "<", List.of(5, 5)),
+                new InfixPrefixTestCase("5 == 5;", "==", List.of(5, 5)),
+                new InfixPrefixTestCase("5 != 5;", "!=", List.of(5, 5))
+        );
+    }
 
-        static Stream<TestCase> infixTestCases() {
-            return Stream.of(
-                    new TestCase("5 + 5;", "+", List.of(5, 5)),
-                    new TestCase("5 - 5;", "-", List.of(5, 5)),
-                    new TestCase("5 * 5;", "*", List.of(5, 5)),
-                    new TestCase("5 / 5;", "/", List.of(5, 5)),
-                    new TestCase("5 > 5;", ">", List.of(5, 5)),
-                    new TestCase("5 < 5;", "<", List.of(5, 5)),
-                    new TestCase("5 == 5;", "==", List.of(5, 5)),
-                    new TestCase("5 != 5;", "!=", List.of(5, 5))
-            );
-        }
+    @ParameterizedTest
+    @MethodSource("infixTestCases")
+    void testParsingInfixExpressions(InfixPrefixTestCase tt) {
+        initialise(tt.input);
 
-        @ParameterizedTest
-        @MethodSource("infixTestCases")
-        void testParsingInfixExpressions(TestCase tt) {
-            initialise(tt.input);
+        // Assert that there is exactly one statement in the program
+        assertEquals(1, program.statements.size(), "program.Statements does not contain 1 statement");
 
-            // Assert that there is exactly one statement in the program
-            assertEquals(1, program.statements.size(), "program.Statements does not contain 1 statement");
+        var stmt = (ExpressionStatement) program.statements.get(0);
+        assertTrue(stmt.getExpression() instanceof InfixExpression,
+                "exp is not a InfixExpression");
 
-            var stmt = (ExpressionStatement) program.statements.get(0);
-            assertTrue(stmt.getExpression() instanceof InfixExpression,
-                    "exp is not a InfixExpression");
+        InfixExpression exp = (InfixExpression) stmt.getExpression();
+        assertEquals(tt.operator, exp.getOperator(), "exp.Operator does not match expected value");
 
-            InfixExpression exp = (InfixExpression) stmt.getExpression();
-            assertEquals(tt.operator, exp.getOperator(), "exp.Operator does not match expected value");
+        assertTrue(testIntegerLiteral(exp.getLeft(), tt.operands.get(0)));
+        assertTrue(testIntegerLiteral(exp.getRight(), tt.operands.get(1)));
+    }
 
-            assertTrue(testIntegerLiteral(exp.getLeft(), tt.operands.get(0)));
-            assertTrue(testIntegerLiteral(exp.getRight(), tt.operands.get(1)));
-        }
+    static Stream<OperatorPrecedenceTestCase> operatorPrecedenceTestCases() {
+        return Stream.of(
+                new OperatorPrecedenceTestCase("a == b", "(a == b)"),
+                new OperatorPrecedenceTestCase("-a * b", "((-a) * b)"),
+                new OperatorPrecedenceTestCase("!-a", "(!(-a))"),
+                new OperatorPrecedenceTestCase("a + b + c", "((a + b) + c)"),
+                new OperatorPrecedenceTestCase("a + b - c", "((a + b) - c)"),
+                new OperatorPrecedenceTestCase("a * b * c", "((a * b) * c)"),
+                new OperatorPrecedenceTestCase("a * b / c", "((a * b) / c)"),
+                new OperatorPrecedenceTestCase("a + b / c", "(a + (b / c))"),
+                new OperatorPrecedenceTestCase("a + b * c + d / e - f", "(((a + (b * c)) + (d / e)) - f)"),
+                new OperatorPrecedenceTestCase("3 + 4; -5 * 5", "(3 + 4)((-5) * 5)"),
+                new OperatorPrecedenceTestCase("5 > 4 == 3 < 4", "((5 > 4) == (3 < 4))"),
+                new OperatorPrecedenceTestCase("5 < 4 != 3 > 4", "((5 < 4) != (3 > 4))"),
+                new OperatorPrecedenceTestCase("3 + 4 * 5 == 3 * 1 + 4 * 5", "((3 + (4 * 5)) == ((3 * 1) + (4 * 5)))")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("operatorPrecedenceTestCases")
+    void testOperatorPrecedenceParsing(OperatorPrecedenceTestCase tt) {
+        initialise(tt.input);
+        assertEquals(tt.expected, program.string());
+    }
 }
+
 
