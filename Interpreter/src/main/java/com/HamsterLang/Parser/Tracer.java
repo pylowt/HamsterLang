deeefc1c72f607f1b857734f462f4d24915077a7
Paul <paul.lowther01@hotmail.com>
2025-03-23 09:38:31 +0000

    Add tracer class

---
+++ Interpreter/src/main/java/com/HamsterLang/Parser/Tracer.java
@@ -0,0 +1,34 @@
+package com.HamsterLang.Parser;
+
+public class Tracer {
+    private int traceLevel = 0;
+    private static final String TRACE_IDENT_PLACEHOLDER = "\t";
+
+    public String indentLevel(){
+        return TRACE_IDENT_PLACEHOLDER.repeat(traceLevel-1);
+    }
+
+    public void tracePrint(String message){
+        System.out.println(indentLevel() + message);
+    }
+
+    private void incIdent(){
+        traceLevel++;
+    }
+
+    private void decIdent(){
+        traceLevel--;
+    }
+
+    public String trace (String message){
+        incIdent();
+        tracePrint(message);
+        return message;
+    }
+
+    public void untrace(String message){
+        tracePrint("END" + message);
+        decIdent();
+    }
+
+}
