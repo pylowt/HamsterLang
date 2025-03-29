d1ed202f698b65f7186667286c17a4cc8087a2c8
Paul <paul.lowther01@hotmail.com>
2025-03-29 16:26:59 +0000

    Add IfExpression node with string and tokenLiteral methods

--- Interpreter/src/main/java/com/HamsterLang/Ast/IfExpression.java
+++ Interpreter/src/main/java/com/HamsterLang/Ast/IfExpression.java
@@ -1,15 +1,54 @@
 package com.HamsterLang.Ast;
 
+import com.HamsterLang.Tokens.Token;
+
 public class IfExpression extends Expression {
-    public ASTRoot getConsequence() {
-        return new ASTRoot();
-    }
+    private Token token; // The 'if' token
+    private Expression condition;
+    private BlockStatement consequence;
+    private BlockStatement alternative;
+
 
     public Expression getCondition() {
-        return new Expression();
+        return condition;
     }
 
-    public Expression getAlternative() {
-        return new Expression();
+    public void setCondition(Expression condition) {
+        this.condition = condition;
+    }
+
+    public BlockStatement getConsequence() {
+        return consequence;
+    }
+
+    public void setConsequence(BlockStatement consequence) {
+        this.consequence = consequence;
+    }
+
+    public BlockStatement getAlternative() {
+        return alternative;
+    }
+
+    public void setAlternative(BlockStatement alternative) {
+        this.alternative = alternative;
+    }
+
+    @Override
+    public String tokenLiteral() {
+        return token.Literal;
+    }
+
+    @Override
+    public String string() {
+        StringBuilder out = new StringBuilder();
+        out.append("if ");
+        out.append(condition.string());
+        out.append(" ");
+        out.append(consequence.string());
+        if (alternative != null) {
+            out.append(" else ");
+            out.append(alternative.string());
+        }
+        return out.toString();
     }
 }
